22c19d0d98ade409ae2240715b1b636ffbc1a340
Matthew Wu <mattwu17@umd.edu>
2023-02-03 18:45:07 -0500

    implement basic map functions

---
+++ scripts/mapview.js
@@ -0,0 +1,76 @@
+let UNIT = 80;
+let mapW = 3696;
+let mapH = 3856;
+
+var inMenu = false;
+var charW = 1848;
+var charH = 1968;
+
+function spawnChar() {
+  var char = document.getElementById("char");
+  char.style.marginLeft = window.innerWidth / 2 - 43 + "px";
+  char.style.marginTop = window.innerHeight / 2 + 3 + "px";
+
+  resizeBackground();
+}
+
+function handleInput(event) {
+  if (event.key === "Enter") {
+    console.log("You pressed enter.");
+  } else if (event.key === "w") {
+    moveUp();
+  } else if (event.key === "a") {
+    moveLeft();
+  } else if (event.key === "s") {
+    moveDown();
+  } else if (event.key === "d") {
+    moveRight();
+  }
+
+  //console.log(event.key)
+}
+
+function moveUp() {
+  var marginTop = document.getElementById("map").style.marginTop;
+  var newMargin = parseInt(marginTop.substring(0, marginTop.length - 2)) + UNIT;
+  document.getElementById("map").style.marginTop = newMargin + "px";
+  charH -= UNIT;
+}
+
+function moveDown() {
+  var marginTop = document.getElementById("map").style.marginTop;
+  var newMargin = parseInt(marginTop.substring(0, marginTop.length - 2)) - UNIT;
+  document.getElementById("map").style.marginTop = newMargin + "px";
+  charH += UNIT;
+}
+
+function moveLeft() {
+  var marginLeft = document.getElementById("map").style.marginLeft;
+  var newMargin =
+    parseInt(marginLeft.substring(0, marginLeft.length - 2)) + UNIT;
+  document.getElementById("map").style.marginLeft = newMargin + "px";
+  charW -= UNIT;
+}
+
+function moveRight() {
+  var marginLeft = document.getElementById("map").style.marginLeft;
+  var newMargin =
+    parseInt(marginLeft.substring(0, marginLeft.length - 2)) - UNIT;
+  document.getElementById("map").style.marginLeft = newMargin + "px";
+  charW += UNIT;
+}
+
+function resizeBackground() {
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+
+  document.getElementById("map").style.marginLeft =
+    charW * -1 + (w - UNIT) / 2 + "px";
+  document.getElementById("map").style.marginTop =
+    charH * -1 + (h - UNIT) / 2 + "px";
+  console.log("test");
+}
+
+window.onload = spawnChar;
+window.onresize = spawnChar;
+window.onkeydown = handleInput;
