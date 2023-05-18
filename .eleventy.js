cd9e6319d7e0ae6abdd57ae0fdad99ce59eea0f1
Matthew Wu <mattwu17@umd.edu>
2023-05-17 20:59:44 -0400

    Added CMS stuff

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy("./src/scripts");
   eleventyConfig.addPassthroughCopy("./src/styles");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 
   return {
     dir: {
